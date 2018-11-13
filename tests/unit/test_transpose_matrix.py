d036894bc2dc98371e6a3280b19984e56500ae8f
Ryo Sumasu <ryo.sms@gmail.com>
2018-11-13 15:09:36 +0900

    Mod display for expected value

--- tests/unit/test_transpose_matrix.py
+++ tests/unit/test_transpose_matrix.py
@@ -6,5 +6,7 @@ def test_can_transpose():
     actual = sut.transpose_matrix([[1, 4, 7],
                                    [2, 5, 8],
                                    [3, 6, 9]])
-    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expected = [1, 2, 3,
+                4, 5, 6,
+                7, 8, 9]
     assert actual == expected
