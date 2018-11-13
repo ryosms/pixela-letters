fece2971fcb32ecce09dde3484e8078f4c12b1f8
Ryo Sumasu <ryo.sms@gmail.com>
2018-11-13 16:25:03 +0900

    Mod entry point

--- pixela_letters/pixela.py
+++ pixela_letters/pixela.py
@@ -4,23 +4,26 @@ from datetime import datetime, timedelta
 import dotenv
 import requests
 
-from pixela_letters.date_converter import find_last_monday, convert_to_ymd_string
+from letters import Letters
+from pixela_letters.date_converter import find_last_start_date, convert_to_ymd_string
 
 
 def run():
     dotenv.load_dotenv()
     user_name = os.environ.get('PIXELA_USER')
     token = os.environ.get('PIXELA_TOKEN')
-    graph_name = "hoge"
+    graph_name = "letters"
 
-    graphs = []
-    target_date = find_last_monday(datetime.now() - timedelta(days=len(graphs)))
+    letters = Letters()
+    graphs = letters.create_quantities('CONGRATULATIONS!')
+    target_date = find_last_start_date(datetime.now() - timedelta(days=len(graphs)))
     endpoint = f"https://pixe.la/v1/users/{user_name}/graphs/{graph_name}"
     header = {'X-USER-TOKEN': token}
 
     for q in graphs:
+        quantity = ((q * 4) + 1) * 5  # min: 5, max: 25
         date = convert_to_ymd_string(target_date)
-        payload = {'date': f"{date}", 'quantity': f"{q}"}
+        payload = {'date': f"{date}", 'quantity': f"{quantity}"}
         response = requests.post(endpoint, headers=header, json=payload)
         print(f"{date}: {response.status_code}")
         target_date = target_date + timedelta(days=1)
