6cbbb2933f9c34e26f79702043255650d01e4dfc
Ryo Sumasu <ryo.sms@gmail.com>
2018-11-15 11:30:37 +0900

    Start impl lowercase

--- pixela_letters/letters/alphabet_lowercase.py
+++ pixela_letters/letters/alphabet_lowercase.py
@@ -1,2 +1,211 @@
 class AlphabetLowercase(object):
-    pass
+
+    @staticmethod
+    def a():
+        """
+        . . . .
+        . . . .
+        . # # .
+        # . . #
+        . # . #
+        # . # #
+        . # . #
+        """
+        return [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+            [0, 1, 0, 1],
+            [1, 0, 1, 1],
+            [0, 1, 0, 1],
+        ]
+
+    @staticmethod
+    def b():
+        """
+        # . . .
+        # . . .
+        # . . .
+        # # # .
+        # . . #
+        # . . #
+        # # # .
+        """
+        return [
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 0],
+        ]
+
+    @staticmethod
+    def c():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def d():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def e():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def f():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def g():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def h():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def i():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def j():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def k():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def l():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def m():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def n():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def o():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def p():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def q():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def r():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def s():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def t():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def u():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def v():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def w():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def x():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def y():
+        """
+        """
+        # TODO: impl
+        return []
+
+    @staticmethod
+    def z():
+        """
+        """
+        # TODO: impl
+        return []
